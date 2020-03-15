567a57f3659702a2c361d96b5a90a80dcad0a2c3
fabien102 <f.benoist@epfl.ch>
2020-03-15 10:53:59 +0100

    28% accuracy Pytorch

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 # import the necessary packages
+import Softmax.data_utils as du
 import argparse
 import numpy as np
-from Softmax.linear_classifier import Softmax
-import Softmax.data_utils as du
 import torch
+from Softmax.linear_classifier import Softmax
 from Net import Net
 import pickle
-
 
 #########################################################################
 # TODO:                                                                 #
@@ -23,6 +22,10 @@ def predict_usingPytorch(X):
     # - Do the operation required to get the predictions                    #
     # - Return predictions in a numpy array                                 #
     #########################################################################
+    
+    
+    X = np.reshape(X, (X.shape[0], -1))
+
     checkpoint = torch.load("Pytorch/model.ckpt",map_location=lambda storage, loc: storage)
     
     
@@ -31,14 +34,10 @@ def predict_usingPytorch(X):
     
     net.load_state_dict(checkpoint)
     
-    X =torch.from_numpy(X)
-    X=X.float()
     predicted = net.predict(X).data
-    #prediction = checkpoint(X)
-
+    
     scores=predicted.numpy()
     y_pred = scores.argmax(axis=1)
-
     #########################################################################
     #                       END OF YOUR CODE                                #
     #########################################################################
@@ -51,7 +50,6 @@ def predict_usingSoftmax(X):
     # - Do the operation required to get the predictions                    #
     # - Return predictions in a numpy array                                 #
     #########################################################################
-    
     with open('Softmax/softmax_weights.pkl', 'rb') as f:
         W = pickle.load(f)
         
@@ -71,15 +69,18 @@ def predict_usingSoftmax(X):
 def main(filename, group_number):
 
     X,Y = du.load_CIFAR_batch(filename)
-    print(X.shape, Y.shape)
+    ### Modified this part
+    mean_pytorch = np.array([0.4914, 0.4822, 0.4465])
+    std_pytorch = np.array([0.2023, 0.1994, 0.2010])
+    X_pytorch = np.divide(np.subtract( X , mean_pytorch[np.newaxis,np.newaxis,:]), std_pytorch[np.newaxis,np.newaxis,:])
+    prediction_pytorch = predict_usingPytorch(torch.Tensor(np.moveaxis(X_pytorch,-1,1)))
+    ####
     X = np.reshape(X, (X.shape[0], -1))
     mean_image = np.mean(X, axis = 0)
     X -= mean_image
-    prediction_pytorch = predict_usingPytorch(X)
     X = np.hstack([X, np.ones((X.shape[0], 1))])
     prediction_softmax = predict_usingSoftmax(X)
     acc_softmax = sum(prediction_softmax == Y)/len(X)
-    print(prediction_pytorch.shape, Y.size)
     acc_pytorch = sum(prediction_pytorch == Y)/len(X)
     print("Group %s ... Softmax= %f ... Pytorch= %f"%(group_number, acc_softmax, acc_pytorch))
 
@@ -89,3 +90,6 @@ if __name__ == "__main__":
     ap.add_argument("-g", "--group", required=True, help="group number")
     args = vars(ap.parse_args())
     main(args["test"],args["group"])
+
+
+
